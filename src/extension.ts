364633cf24fcadeaf5aa971f1d32bed7cdfc396a
imransh8 <shaheer.imran@mail.utoronto.ca>
2025-08-18 23:34:09 +0500

    added set api key functionality

--- src/extension.ts
+++ src/extension.ts
@@ -1,3 +1,13 @@
+// run in terminal to push latest changes to github
+
+// git add .
+// git commit -m "Describe what you changed"
+// or
+// git commit -m "Updated extension.ts"
+// git push origin main
+
+
+
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
@@ -10,9 +20,29 @@ type ReviewIssue = {
   suggestion?: string; // ? to make it optional 
 };
 
+let diagCollection: vscode.DiagnosticCollection; // this is how vs code shows errors and squiggly lines
+
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+
+	const diagCollection = vscode.languages.createDiagnosticCollection('deep-code-review'); // creates a collection for all the review problems
+	context.subscriptions.push(diagCollection); // this line makes vs code clean it up when reloading/unloading
+
+	context.subscriptions.push(vscode.commands.registerCommand('deep-code-reviewer.setOpenAIKey', async () => {
+		const key = await vscode.window.showInputBox({
+			prompt: 'Enter your OpenAI API key',
+			password: true
+		});
+		if (!key) {return;}
+		await context.secrets.store('deepCode.openai.apiKey', key);
+		vscode.window.showInformationMessage('OpenAI API key saved securely.');
+	}));
+	
+	// some notes for the code above 
+	// async allows us to use await
+	// we use await becase a user takes time to enter a password
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
